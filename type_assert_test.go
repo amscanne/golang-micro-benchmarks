d8cd9a27d2c83ab600f42619911e8ec6ba6236e6
Adin Scannell <ascannell@google.com>
2014-10-23 12:06:24 -0700

    Adding a few tests.

---
+++ type_assert_test.go
@@ -0,0 +1,18 @@
+package benchmarks
+
+import (
+    "testing"
+)
+
+func do_type_assert(N int, value interface{}, output *int) {
+    for i := 0; i < N; i++ {
+	v := value.(int)
+        *output += v
+    }
+}
+
+func BenchmarkTypeAssert(b *testing.B) {
+    var value int
+    do_type_assert(b.N, int(1), &value)
+    do_type_assert(b.N, int(-1), &value)
+}
