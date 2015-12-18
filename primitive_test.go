3fb281be5836661f084298669be9f2f98d185624
Adin Scannell <ascannell@google.com>
2015-12-18 14:27:44 -0800

    Add primitive test.

---
+++ primitive_test.go
@@ -0,0 +1,41 @@
+package benchmarks
+
+import (
+	"testing"
+)
+
+type Opaque struct {
+    val int
+}
+
+func (o Opaque) Add(other Opaque) Opaque {
+    return Opaque{o.val + other.val}
+}
+
+func (o *Opaque) AddPtr(other *Opaque) Opaque {
+    return Opaque{o.val + other.val}
+}
+
+func BenchmarkStructOpByValue(b *testing.B) {
+        c := Opaque{0}
+        inc := Opaque{1}
+	for i := 0; i < b.N; i++ {
+            c = c.Add(inc)
+        }
+}
+
+func BenchmarkStructOpByPtr(b *testing.B) {
+        c := Opaque{0}
+        inc := &Opaque{1}
+	for i := 0; i < b.N; i++ {
+            c = c.AddPtr(inc)
+        }
+}
+
+func BenchmarkPrimtiveOp(b *testing.B) {
+        c := 0
+        inc := 1
+	for i := 0; i < b.N; i++ {    
+            c = c + inc
+        }
+}
