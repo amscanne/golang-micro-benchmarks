da7dbc86c4b971d1d8e9b203eed63648ab0e0c1c
Adin Scannell <adin@scannell.ca>
2019-01-03 13:39:20 -0800

    Fix function benchmarks.

--- func_test.go
+++ func_test.go
@@ -4,20 +4,47 @@ import (
 	"testing"
 )
 
-func do_func(N int, value bool, output *int) {
-	if value {
-		for i := 0; i < N; i++ {
-			add(output)
-		}
-	} else {
-		for i := 0; i < N; i++ {
-			subtract(output)
-		}
+func simple_func(output *int, value bool) {
+	*output++
+	if value { // Never run.
+		simple_func(output, value)
 	}
 }
 
-func BenchmarkFunc(b *testing.B) {
+//go:nosplit
+func nosplit_func(output *int, value bool) {
+	*output++
+	if value { // Never run.
+		simple_func(output, value)
+	}
+}
+
+func baseline(output *int, value bool) {
+	*output++
+	if value {
+		// Never run. We want the cost of the comparison, but don't
+		// make this recursive because we still want it to be inlined.
+		*output++
+	}
+}
+
+func BenchmarkBaselineFunc(b *testing.B) {
 	var value int
-	do_struct(b.N, true, &value)
-	do_struct(b.N, false, &value)
+	for i := 0; i < b.N; i++ {
+		baseline(&value, false)
+	}
+}
+
+func BenchmarkNormalFunc(b *testing.B) {
+	var value int
+	for i := 0; i < b.N; i++ {
+		simple_func(&value, false)
+	}
+}
+
+func BenchmarkNosplitFunc(b *testing.B) {
+	var value int
+	for i := 0; i < b.N; i++ {
+		nosplit_func(&value, false)
+	}
 }
